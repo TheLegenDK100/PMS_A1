40c504e7206a6e9cf7679985ef3e77cb077bfa0b
TheLegenDK100 <154776686+TheLegenDK100@users.noreply.github.com>
2026-04-04 21:54:21 +0800

    Delete Part2 directory

--- Part2/src/app/components/help/help.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-help',
-  templateUrl: './help.component.html'
-})
-export class HelpComponent {}
--- Part2/src/app/components/privacy-security/privacy-security.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-privacy-security',
-  templateUrl: './privacy-security.component.html'
-})
-export class PrivacySecurityComponent {}
